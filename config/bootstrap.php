c652c6e7577f0053a7590bb20ac12fdb3558da0c
nojimage <nojimage@gmail.com>
2017-06-16 17:44:43 +0900

    fixes bootstrap

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -10,18 +10,12 @@ use Cake\Core\Configure;
 
 if (file_exists(CONFIG . 'hybridauth.php')) {
     Configure::load('hybridauth');
-} else {
-    $config = [
-        'providers' => [
-            'OpenID' => [
-                'enabled' => true
-            ]
-        ],
-        'debug_mode' => (bool)Configure::read('debug'),
-        'debug_file' => LOGS . 'hybridauth.log',
-    ];
-    Configure::write('HybridAuth', $config);
 }
+
+if (!Configure::check('HybridAuth')) {
+    throw new InvalidArgumentException(__("Can't found HybridAuth configuration."));
+}
+
 if (file_exists(CONFIG . 'hybridauth_local.php')) {
     Configure::load('hybridauth_local');
 }
